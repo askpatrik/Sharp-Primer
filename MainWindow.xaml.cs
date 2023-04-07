c94bb3fd0464ef9aae227e89e2cb8380cdb7a2ca
Patrik Ask <ask.patrik@hotmail.com>
2023-04-07 12:02:15 +0200

    Added title text on MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Sharp_Primer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
