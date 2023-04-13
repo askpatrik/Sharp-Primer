7a35d6a8cb139a33a0039b5bf454b9bc25af9acb
Patrik Ask <ask.patrik@hotmail.com>
2023-04-13 11:23:17 +0200

    added properties

--- Challenges/Challenge.cs
+++ Challenges/Challenge.cs
@@ -7,11 +7,22 @@ namespace Sharp_Primer.Challenges
     {
         public string Title { get; }
         public string Description { get; }
+        public int Input1 {get;}
+        public int Input2 {get;}
+        public int ExpectedOutput { get; set; }
 
-        public Challenge(string title, string description)
+        public Challenge(string title, string description, int input1, int input2, int expectedOutput)
         {
             Title = title;
             Description = description;
+            Input1 = input1;
+            Input2 = input2;
+            ExpectedOutput = expectedOutput;
+        }
+
+        public bool CheckAnswer(int answer)
+        {
+            return answer == ExpectedOutput;
         }
 
         public static Challenge GetRandomChallenge()
@@ -19,11 +30,8 @@ namespace Sharp_Primer.Challenges
             Random random = new Random();
             List<Challenge> challenges = new List<Challenge>()
     {
-        new Challenge("Challenge 1", "Write a program that outputs the numbers from 1 to 100. But for multiples of three, output “Fizz” instead of the number and for the multiples of five, output “Buzz”. For numbers which are multiples of both three and five, output “FizzBuzz”."),
-        new Challenge("Challenge 2", "Write a program that takes a string input from the user and outputs the string reversed."),
-        new Challenge("Challenge 3", "Write a program that takes an integer input from the user and outputs whether the number is even or odd."),
-        new Challenge("Challenge 4", "Write a program that takes two integer inputs from the user and outputs their sum, difference, product, and quotient."),
-        new Challenge("Challenge 5", "Write a program that takes an integer input from the user and outputs the factorial of that number.")
+        new Challenge("Sum of two integers", "Write a program that takes two integer inputs and returns their sum", 5, 10, 15)
+    
         // Add more challenges here
     };
             int index = random.Next(challenges.Count);
